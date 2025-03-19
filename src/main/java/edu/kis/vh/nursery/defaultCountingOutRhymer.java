73f8a082292b034dfecb2ab501e3fa3520d10611
Oskar <251250@edu.p.lodz.pl>
2025-03-19 14:56:18 +0100

    5. poprawa nazw pól klas

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    private int[] numbersArray = new int[12];
 
-    public int total = -1;
+    public int totalIndex = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbersArray[++totalIndex] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return totalIndex == -1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return totalIndex == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return NUMBERS[total];
+        return numbersArray[totalIndex];
     }
 
     public int countOut() {
         if (callCheck())
             return -1;
-        return NUMBERS[total--];
+        return numbersArray[totalIndex--];
     }
 
 }
