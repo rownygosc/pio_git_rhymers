87b021fd3b6f6db20ad4f5294e8a2ab164204e7b
Oskar <251250@edu.p.lodz.pl>
2025-04-08 21:40:58 +0200

    15. projekt jest poprawny

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,6 +5,8 @@ import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
+/*projekt jes poprawny*/
+
 class RhymersDemo {
 
     private static final int INPUT_LOOP_LIMIT = 15;
