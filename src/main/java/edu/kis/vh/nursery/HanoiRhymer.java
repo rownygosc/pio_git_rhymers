d0cbb82831660ec667ef10e038c0e1d6173e10cd
adriannakostrzewa <251170@edu.p.lodz.pl>
2025-04-08 20:39:13 +0200

    8. dodanie @Override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -12,6 +12,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
         return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
